f486c88a7ff12686c41b18c4c69bbd75f097a069
Martavias Cary <martavias.cary@outlook.com>
2022-11-05 03:21:19 -0400

    edit photos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import star from "../../public/img/star.png";
 
 export default function Card(props) {
-  const image = `${props.img}`;
   let badgeText;
   if (props.cardData.openSpots === 0) {
     badgeText = "SOLD OUT";
@@ -13,7 +12,7 @@ export default function Card(props) {
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={image} className="card--image" />
+      <img src={props.img} className="card--image" />
       <div className="card--stats">
         <img src={star} className="card--star" />
         <span>{props.cardData.stats.rating}</span>
